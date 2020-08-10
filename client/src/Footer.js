a5218327762c9daf835eac659ddb3904c5c01d23
Prem <premkamban@gmail.com>
2020-08-10 13:59:02 +0530

    removed form from html

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import Form from './Form'
 
 export default class Footer extends Component {
     render() {
@@ -22,24 +21,19 @@ export default class Footer extends Component {
                     <div className="container">
                         <div className="row">
                             <div className="col-12" data-aos="zoom-in-right">
-                                <h1 className="section-title-5 text-md-left text-center bold-h-h">Contact us</h1>
+                                <h1 className="section-title-5 text-center bold-h-h">Contact us</h1>
                             </div>
                         </div>
-                        <div className="row mt-5 mb-5">
-                            <div className="col-md-6 col-12" data-aos="zoom-in-right">
-                                <Form />
-                            </div>
-                            <div className="col-md-6 col-12 text-center" data-aos="zoom-in-left">
-                                <div className="ml-md-5 pt-5 mt-5 pt-md-3 mt-md-0">
-                                    <img src={require("./assert/img/address.png")} className="img-fluid w-50 mb-4" alt="Footer" />
-                                    <p><strong>Email :</strong> <a href="mailto:support@bicesterbirds4sale.co.uk" style={style.color}>support@bicesterbirds4sale.co.uk</a></p>
-                                    <p><strong>Phone :</strong> <a href="tel:07553 356460
-    
-    " style={style.phone}>07553 356460
-    </a></p>
+                        <div className="row mt-md-5 mt-3 mb-3 mt-mb-5">
+
+                            <div className="col-md-12 col-12 text-center" data-aos="zoom-in-left">
+                                <div className="  mt-5 pt-md-3 mt-md-0">
+                                    <img src={require("./assert/img/address.png")} className="img-fluid w-25 mb-4" />
+                                    <p><strong>Email :</strong> <a href="mailto:support@bicesterbirds4sale.co.uk" style={style.ph}>support@bicesterbirds4sale.co.uk</a></p>
+                                    <p><strong>Phone :</strong> <a href="tel:07553 356460" style={style.phone}>07553 356460 </a></p>
                                     <p><strong>Address :</strong> 31 Redcar Road, Bicester, OX26 1BQ</p>
                                 </div>
-                                <div className="ml-md-5 mt-5  pt-md-3 mt-md-0" >
+                                <div className=" mt-5  pt-md-3 mt-md-0" >
                                     <h5>Follow us:</h5>
                                     <a href="https://www.facebook.com/BicesterBirds/" target="_blank" className="fa fa-facebook"></a>
                                     <a href="https://www.instagram.com/bicesterbirds/" target="_blank" className="ml-3 fa fa-instagram"></a>
@@ -48,12 +42,6 @@ export default class Footer extends Component {
                         </div>
                     </div>
                 </section>
-
-
-                <footer className="footer pt-4 pb-3 " >
-                    <p className="text-center mb-0" style={style.FooterLine}>Bicester birds Copyright 2020 All rights reserved</p>
-                </footer>
-
             </React.Fragment>
         )
     }
